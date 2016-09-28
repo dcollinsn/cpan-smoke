9d14ce2662ecf03b343bcd7847999bb30f76d36d
Dan Collins <dcollinsn@gmail.com>
2016-09-28 18:26:00 -0400

    Add completed module count to stat

--- stat.pl
+++ stat.pl
@@ -7,8 +7,10 @@ foreach my $i (1..4) {
 
     if ($ps =~ /CPAN::Reporter::Smoker/) {
         print "$user running";
+        my $count = `ls /home/cpan$i/reports/ | wc -l`;
+        printf(' [%5d]', $count);
         if ($ps =~ /(\d{2}:\d{2})\s+pts.\d+\s+[\d:]{8}.+install\( '(.+?)'/) {
-            print ", [$1] started $2";
+            print ", [$1] $2";
         }
     } else {
         print "$user not running";
