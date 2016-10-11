0627bc53c57450d419984b7742c5196fbbb6df9e
Dan Collins <dcollinsn@gmail.com>
2016-10-11 17:24:57 -0400

    Add better version name detection

--- tellversion.sh
+++ tellversion.sh
@@ -1,4 +1,4 @@
 version=$(/home/cpan$1/install/bin/perl -v | perl -ne '/.+\((v.+?)\)/ && print $1 || ""')
 head=$(cat /home/cpan$1/perl/.git/HEAD)
 dirname=$(perl -e 'if ($ARGV[1]=~m|^.+/(.+)|) {$a=$ARGV[0];$b=$1;$a=~s/-/-$b-/;print $a} else {print $ARGV[0]}' "$version" "$head")
-mkdir $dirname && /home/cpan$1/install/bin/perl -V > ./$dirname/perl-V && cp /home/cpan$1/reports/* ./$dirname/
+echo "cpan$1: $dirname"
